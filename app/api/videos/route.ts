89fbb7a0eb696073cca134673bba1a117e50cff0
yash <yashg5304@gmail.com>
2026-06-21 14:32:11 +0530

    prisma and neonDb setup

---
+++ app/api/videos/route.ts
@@ -0,0 +1,31 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function GET(request:NextRequest) {
+    try {
+        
+      const videos = await prisma.video.findMany({
+          orderBy: {
+            createdAt: "desc"
+          }
+        })
+        return NextResponse.json(videos)
+
+    } catch (error) {
+        return NextResponse.json({error: "\Error fetching videos"},{status: 500});
+    }finally{
+      await prisma.$disconnect()
+    }
+}
